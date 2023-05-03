fd48cfc3c5ab10dcde5c486d2812dd734628cf37
David Wilson <david@daviwil.com>
2023-05-03 15:19:03 +0300

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((org-link-file-path-type . relative))))
